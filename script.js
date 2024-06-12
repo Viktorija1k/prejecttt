35a33c7bc88aee57c5883ea9766e01b73947869d
Viktorija1k <vika4a5@inbox.lv>
2024-06-12 17:55:11 +0000

    canva un JS funkcijas

---
+++ script.js
@@ -0,0 +1,47 @@
+window.onload =function() 
+{
+  
+  var canvas = document.getElementById("zimejums");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "green";
+  ctx.fillRect(20, 25, 100, 100);
+}
+function taisni() {
+var canvas = document.getElementById("zimejums");
+var ctx = canvas.getContext("2d");
+ctx.strokeStyle = "purple";
+ctx.strokeRect(60, 65, 100, 100);
+}
+function aplis() {
+var canvas = document.getElementById("zimejums");
+var konteksts = canvas.getContext("2d");
+konteksts.beginPath();
+konteksts.arc(200, 200, 50, 0, 2 * Math.PI)
+  konteksts.lineWidth = 13;
+  konteksts.fillStyle = "yellow";
+  konteksts.strokeStyle = "red";
+  konteksts.fill();
+  konteksts.stroke();
+}
+function linija() {
+var canvas = document.getElementById("zimejums");
+var konteksts = canvas.getContext("2d");
+  konteksts.beginPath();
+  konteksts.moveTo(20, 20);
+  konteksts.lineTo(60, 60);
+  konteksts.lineWidth =5;
+  konteksts.strokeStyle = "blue";
+  konteksts.stroke();
+}
+function text() {
+  var canvas = document.getElementById("zimejums");
+  var konteksts = canvas.getContext("2d");
+    konteksts.font = "30px Arial";
+    konteksts.fillStyle = "black";
+    konteksts.fillText("Sveiki, pasaule!", 75, 100)
+    konteksts.lineWidth = 5;
+
+}
+  
+  
+
